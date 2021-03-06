b371a79dc9a887f178a9f80b86ae711b3efa38d6
Fabian Trampusch <fabian.trampusch@freenet.de>
2021-03-06 14:28:50 +0100

    basic express hello world

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("hello boilerplate");
+import express from "express";
+
+const app = express();
+const port = 3000;
+app.get("/", (_req, res) => {
+  res.send("hello boilerplate");
+});
+app.listen(port, () => {
+  console.log(`server is listening on ${port}`);
+});
