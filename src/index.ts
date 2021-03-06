007f0d1d51febd169d880ae04f9a1f00ecbff973
Fabian Trampusch <fabian.trampusch@freenet.de>
2021-03-06 14:39:13 +0100

    use handlebars for view templating

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import express from "express";
 
 const app = express();
 const port = 3000;
+
+app.set("view engine", "hbs");
+
 app.get("/", (_req, res) => {
-  res.send("hello boilerplate");
+  res.render("index", { name: "test" });
 });
 app.listen(port, () => {
   console.log(`server is listening on ${port}`);
